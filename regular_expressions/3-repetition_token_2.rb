7c932b06f47c9fc846c589eb8d70e8943819acc4
sando2004 <s.mugwaneza@alustudent.com>
2025-05-09 14:24:29 -0700

    DDS

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-
-# Accept the input string from the command line
-input = ARGV[0]
-
-# Match strings like hbtn, hbtttttn
-puts input.scan(/^hbt+n$/).join
+puts ARGV[0].scan(/^hbt+n$/).join
