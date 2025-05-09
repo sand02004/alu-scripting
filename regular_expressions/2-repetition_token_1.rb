17d9a6ee4aeae2cb3cd2067298109a3f37595f2e
sando2004 <s.mugwaneza@alustudent.com>
2025-05-09 14:08:49 -0700

    SS

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,14 +1,3 @@
 #!/usr/bin/env ruby
-
-# Get the argument passed to the script
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /^hb*tn$/
-
-# Check if the input string matches the pattern
-if input_string.match(pattern)
-  puts "Match found!"
-else
-  puts "No match found."
-end
+# Task 2
+puts ARGV[0].scan(/hb?tn/).join
